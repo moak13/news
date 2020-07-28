b98b85a8fe2f857de158b70f5d472dd7b5761a13
Michael Kalango <michaelkalango@gmail.com>
2020-07-28 18:03:34 +0100

    Feature Layer - (HOME) presentation | view import fix

--- lib/features/home/presentation/view/home_view.dart
+++ lib/features/home/presentation/view/home_view.dart
@@ -1,27 +1,51 @@
 import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
 
+import '../../../../core/widget/indicator.dart';
 import '../viewmodel/home_viewmodel.dart';
 import '../widget/news_card.dart';
 
-class HomeView extends StatelessWidget {
+class HomeView extends StatefulWidget {
   const HomeView({Key key}) : super(key: key);
 
+  @override
+  _HomeViewState createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  GlobalKey<ScaffoldState> _key;
   @override
   Widget build(BuildContext context) {
     return ViewModelBuilder<HomeViewModel>.reactive(
       onModelReady: (model) => model.fetchNews(),
       builder: (context, model, child) {
         return Scaffold(
-            appBar: AppBar(
-              title: Text('Flutter News App'),
-            ),
-            body: ListView.builder(
-              itemCount: model.news.articles.length,
-              itemBuilder: (context, index) {
-                return NewsCard(articles: model.news.articles[index]);
-              },
-            ));
+          key: _key,
+          appBar: AppBar(
+            title: Text('Flutter News App'),
+          ),
+          body: model.isBusy
+              ? Indicator()
+              : !model.hasError
+                  ? ListView.builder(
+                      itemCount: model.news.articles.length,
+                      itemBuilder: (context, index) {
+                        return NewsCard(articles: model.news.articles[index]);
+                      },
+                    )
+                  : _key.currentState.showSnackBar(
+                      SnackBar(
+                        backgroundColor: Colors.red[200],
+                        content: Text(
+                          model.err,
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+        );
       },
       viewModelBuilder: () => HomeViewModel(),
     );
