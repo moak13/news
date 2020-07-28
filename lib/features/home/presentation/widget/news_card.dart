562e6e5bca32d4381745f5dda9c3547096ce08e1
Michael Kalango <michaelkalango@gmail.com>
2020-07-28 17:43:07 +0100

    Feature Layer - (HOME) presentation | widget revamp

--- lib/features/home/presentation/widget/news_card.dart
+++ lib/features/home/presentation/widget/news_card.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:news/core/utils/screen_util.dart';
-import 'package:news/features/home/domain/entities/news_data.dart';
+
+import '../../../../core/utils/screen_util.dart';
+import '../../domain/entities/news_data.dart';
 
 class NewsCard extends StatelessWidget {
   final Articles articles;
@@ -9,10 +10,24 @@ class NewsCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      padding: const EdgeInsets.all(20),
+      decoration: BoxDecoration(boxShadow: [
+        BoxShadow(
+          color: Colors.grey,
+          blurRadius: 5,
+          spreadRadius: 2,
+        )
+      ]),
       height: ScreenUtil(context).setHeight(height: 40),
       width: ScreenUtil(context).setWidth(width: 40),
       child: Center(
-        child: Text(articles.author),
+        child: Text(
+          articles.author,
+          style: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
       ),
     );
   }
