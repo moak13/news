33add40da6469653315951384057d96c00175d04
Michael Kalango <michaelkalango@gmail.com>
2020-07-29 01:13:55 +0100

    Feature Layer - splash viewmodel upgrade

--- lib/features/splash/presentation/viewmodel/splash_viewmodel.dart
+++ lib/features/splash/presentation/viewmodel/splash_viewmodel.dart
@@ -1,6 +1,20 @@
+import 'dart:async';
+
+import 'package:news/core/utils/locator.dart';
 import 'package:stacked/stacked.dart';
+import 'package:stacked_services/stacked_services.dart';
 
 class SplashViewModel extends BaseViewModel {
+  NavigationService _navigationService = locator<NavigationService>();
+
+  // This holds the screen to move to
+  makeMove() {
+    _navigationService.navigateTo('/home-view');
+  }
+
   // This will handle the movement to a new page when app runs
-  startupHandler() {}
+  startupHandler() {
+    Duration _duration = Duration(seconds: 2);
+    return Timer(_duration, makeMove);
+  }
 }
